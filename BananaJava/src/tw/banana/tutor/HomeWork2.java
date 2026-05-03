8d39d80ce390b15d2f8b3afa8c5c9417966cf2c6
banana <x732542@gmail.com>
2026-05-03 21:14:06 +0800

    0503

--- BananaJava/src/tw/banana/tutor/HomeWork2.java
+++ BananaJava/src/tw/banana/tutor/HomeWork2.java
@@ -3,29 +3,38 @@ package tw.banana.tutor;
 public class HomeWork2 {
 	public static void main(String[] args) {
 //test 做出骰子 記錄100次的結果 並列印出每個數字出現的次數 **灌鉛骰子
-		
+
+		// 定義收集100次紀錄的陣列
 		byte[] dicerecords = new byte[100];
-			for (byte i = 0; i < 100; i++) {
-				double temp = Math.random();
-				// temp  *上限 + 下限
-				byte dice = (byte) ((temp * 6) + 1);
-//				System.out.println(dice);
-// 把生出的變數塞進去
-				dicerecords[i] = dice;
-			
+		// 迴圈 運轉100次
+		for (byte i = 0; i < 100; i++) {
+			// 取亂數
+			double temp = Math.random();
+			// temp *上限 + 下限
+			byte dice = (byte) ((temp * 6) + 1);
+			// 把生出的變數塞進去
+			dicerecords[i] = dice;
 		}
+		// 生成新陣列 內容為0~6
 		int[] counts = new int[7];
+
 		for (byte i = 0; i < dicerecords.length; i++) {
 			// 基於骰子特性 可以放到1到6裡面
 //				byte point = dicerecords[i];
 //					counts [point]++;
-			//正常思路 對應 1~6
-			if (dicerecords[i] == 1) counts[1]++;
-			if (dicerecords[i] == 2) counts[2]++;
-			if (dicerecords[i] == 3) counts[3]++;
-			if (dicerecords[i] == 4) counts[4]++;
-			if (dicerecords[i] == 5) counts[5]++;
-			if (dicerecords[i] == 6) counts[6]++;
+			// 正常思路 對應 1~6
+			if (dicerecords[i] == 1)
+				counts[1]++;
+			if (dicerecords[i] == 2)
+				counts[2]++;
+			if (dicerecords[i] == 3)
+				counts[3]++;
+			if (dicerecords[i] == 4)
+				counts[4]++;
+			if (dicerecords[i] == 5)
+				counts[5]++;
+			if (dicerecords[i] == 6)
+				counts[6]++;
 		}
 		System.out.println("1點出現次數：" + counts[1]);
 		System.out.println("2點出現次數：" + counts[2]);
@@ -33,5 +42,7 @@ public class HomeWork2 {
 		System.out.println("4點出現次數：" + counts[4]);
 		System.out.println("5點出現次數：" + counts[5]);
 		System.out.println("6點出現次數：" + counts[6]);
+
+
 	}
 }
--- BananaJava/src/tw/banana/tutor/HomeWork3.java
+++ BananaJava/src/tw/banana/tutor/HomeWork3.java
@@ -3,8 +3,54 @@ package tw.banana.tutor;
 public class HomeWork3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		byte[] records = new byte[100];
+		for (byte i = 0; i < 100; i++) {
+			// 灌鉛骰子 手動控制骰子點數
+			byte pool[] = { 1, 2, 3, 4, 5, 6, 6, 6, 6 };
+			// 隨機選取陣列內的內容
+			int index = (int) (Math.random() * pool.length);
+			// pool[index] 代表：請電腦去 pool 這個抽獎箱裡，把編號為 index 的那顆球拿出來。
+			// byte dice = ... 代表：把拿出來的那顆球（點數），暫時存放在一個叫 dice 的變數裡，準備下一行塞進 aa[i]。
+			byte dice = pool[index];
+			records[i] = dice;
 
+		}
+		int[] count = new int[7];
+		for (byte i = 0; i < records.length; i++) {
+			// 基於骰子特性 可以放到1到6裡面
+			byte point = records[i];
+			count[point]++;
+		}
+		// 用迴圈印出 1 到 6 點的統計結果
+		for (int i = 1; i <= 6; i++) {
+			System.out.println(i + "點出現次數：" + count[i]);
+		}
+		System.out.println();
+
+//----------------------------------------------------------------
+		byte[] record = new byte[100];
+		// 灌鉛骰子 亂數寫法
+		for (byte i = 0; i < 100; i++) {
+			// 用亂數<1的特性 定義機率
+			double r = Math.random();
+			byte dice;
+			// 當亂數抽取到0.49999999 都是6 相當於50%的機率
+			if (r < 0.5) {
+				dice = 6;
+				record[i] = dice;
+			} else {
+				// 公平骰子
+				dice = (byte) (Math.random() * 6 + 1);
+				record[i] = dice;
+			}
+		}
+		int[] counts = new int[7];
+		for (int i = 0; i < record.length; i++) {
+			counts[record[i]]++;
+		}
+		for (int i = 1; i <= 6; i++) {
+			System.out.println(i + "點出現次數：" + counts[i]);
+
+		}
 	}
-
 }
