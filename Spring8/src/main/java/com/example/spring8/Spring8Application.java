96eb01fee88d7c7812c84c32dc5236f83748305f
x732542 <x732542@gmail.com>
2026-07-02 09:21:09 +0800

    0702

---
+++ Spring8/src/main/java/com/example/spring8/Spring8Application.java
@@ -0,0 +1,13 @@
+package com.example.spring8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring8Application.class, args);
+	}
+
+}
---
+++ Spring8/src/test/java/com/example/spring8/Spring8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
