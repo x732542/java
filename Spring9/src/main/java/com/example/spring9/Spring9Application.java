0e3bd8963a0585b2578bd3c975494e0304f6a0e0
x732542 <x732542@gmail.com>
2026-07-06 09:26:20 +0800

    0706

---
+++ Spring9/src/main/java/com/example/spring9/Spring9Application.java
@@ -0,0 +1,13 @@
+package com.example.spring9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring9Application.class, args);
+	}
+
+}
---
+++ Spring9/src/test/java/com/example/spring9/Spring9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
