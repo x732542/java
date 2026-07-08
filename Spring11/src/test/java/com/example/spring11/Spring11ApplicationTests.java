cfaad3cf1bcd12d97cecf3ff9c55bec1ef64b046
x732542 <x732542@gmail.com>
2026-07-08 09:11:36 +0800

    0708

---
+++ Spring11/src/test/java/com/example/spring11/Spring11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
